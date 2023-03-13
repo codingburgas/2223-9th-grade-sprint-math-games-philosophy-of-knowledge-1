697ac070c3e666a8060c9a10f4e217b94b827f1c
KNKostadinov21 <106849849+KNKostadinov21@users.noreply.github.com>
2023-03-13 21:54:23 +0200

    Make switch-case base

--- main.cpp
+++ main.cpp
@@ -19,4 +19,158 @@ int main()
 {
     srand(time(NULL));
     int randomNumber = rand() % 30;
+    Number number("", "");
+
+    switch (randomNumber)
+    {
+        case 1:
+        number.NumberToConvert = "";
+        number.System = "";
+        break;
+
+        case 2:
+        number.NumberToConvert = "";
+        number.System = "";
+        break;
+
+        case 3:
+        number.NumberToConvert = "";
+        number.System = "";
+        break;
+
+        case 4:
+        number.NumberToConvert = "";
+        number.System = "";
+        break;
+
+        case 5:
+        number.NumberToConvert = "";
+        number.System = "";
+        break;
+
+        case 6:
+        number.NumberToConvert = "";
+        number.System = "";
+        break;
+
+        case 7:
+        number.NumberToConvert = "";
+        number.System = "";
+        break;
+
+        case 8:
+        number.NumberToConvert = "";
+        number.System = "";
+        break;
+
+        case 9:
+        number.NumberToConvert = "";
+        number.System = "";
+        break;
+
+        case 10:
+        number.NumberToConvert = "";
+        number.System = "";
+        break;
+
+        case 11:
+        number.NumberToConvert = "";
+        number.System = "";
+        break;
+
+        case 12:
+        number.NumberToConvert = "";
+        number.System = "";
+        break;
+
+        case 13:
+        number.NumberToConvert = "";
+        number.System = "";
+        break;
+
+        case 14:
+        number.NumberToConvert = "";
+        number.System = "";
+        break;
+
+        case 15:
+        number.NumberToConvert = "";
+        number.System = "";
+        break;
+
+        case 16:
+        number.NumberToConvert = "";
+        number.System = "";
+        break;
+
+        case 17:
+        number.NumberToConvert = "";
+        number.System = "";
+        break;
+
+        case 18:
+        number.NumberToConvert = "";
+        number.System = "";
+        break;
+
+        case 19:
+        number.NumberToConvert = "";
+        number.System = "";
+        break;
+
+        case 20:
+        number.NumberToConvert = "";
+        number.System = "";
+        break;
+
+        case 21:
+        number.NumberToConvert = "";
+        number.System = "";
+        break;
+
+        case 22:
+        number.NumberToConvert = "";
+        number.System = "";
+        break;
+
+        case 23:
+        number.NumberToConvert = "";
+        number.System = "";
+        break;
+
+        case 24:
+        number.NumberToConvert = "";
+        number.System = "";
+        break;
+
+        case 25:
+        number.NumberToConvert = "";
+        number.System = "";
+        break;
+
+        case 26:
+        number.NumberToConvert = "";
+        number.System = "";
+        break;
+
+        case 27:
+        number.NumberToConvert = "";
+        number.System = "";
+        break;
+
+        case 28:
+        number.NumberToConvert = "";
+        number.System = "";
+        break;
+
+        case 29:
+        number.NumberToConvert = "";
+        number.System = "";
+        break;
+
+        case 30:
+        number.NumberToConvert = "";
+        number.System = "";
+        break;
+    }
 }
