317060f6264639fdb02403429439fd9b21931448
EGGeorgieva21 <EGGeorgieva21@codingburgas.bg>
2023-03-27 18:53:35 +0300

    Everything oficially works. Major changes everywhere.

--- game/BitCalc/BitCalc/BitCalc.cpp
+++ game/BitCalc/BitCalc/BitCalc.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-#include <main.h>
+#include "main.h"
 using namespace std;
 
 int main()
 {
-
+    // instructions: answer must be in the hexadecimal system.
     //gives you the two numbers
     twoNumberGenerator();
     //gives you the operation
     bitOperationGenerator();
     //user input
-    userInput();
+    userAnswer();
 }
