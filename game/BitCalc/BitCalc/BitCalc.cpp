4016bedf13c4b04465331fd18e871a2ae88c8ca9
EGGeorgieva21 <EGGeorgieva21@codingburgas.bg>
2023-03-28 03:07:03 +0300

    Final touches to code

--- game/BitCalc/BitCalc/BitCalc.cpp
+++ game/BitCalc/BitCalc/BitCalc.cpp
@@ -4,7 +4,9 @@ using namespace std;
 
 int main()
 {
-    // instructions: answer must be in the hexadecimal system.
+    cout << "INSTRUCTIONS: \nYou will receive two numbers in one of these systems: binary, decimal, hexadecimal, as well as a binary funtion.\nCalculate them and check the answer.\nTHE ANSWER MUST BE IN THE DECIMAL SYSTEM" << endl;
+    cout << "-----------------------------------------------------------------------------------------------------------------"<< endl;
+ 
     //gives you the two numbers
     twoNumberGenerator();
     //gives you the operation
